68fdc99eb89a7ae727d1065d7b76aadd92a9fd91
Andrei-Robert <andrei750238@gmail.com>
2024-08-22 00:53:31 +0300

    Started work on september content

--- gen.py
+++ gen.py
@@ -1,12 +1,12 @@
 import datetime
 
-start_date = datetime.date(2024, 8, 1)
-end_date = datetime.date(2024, 8, 31)
+start_date = datetime.date(2024, 9, 1)
+end_date = datetime.date(2024, 9, 30)
 
 for i in range((end_date - start_date).days + 1):
     day = start_date + datetime.timedelta(days=i)
     print('{')
     print(f'    "date": "{day}",')
-    print('    "content": "Te iubesc enorm de mult! Tu esti fericirea mea!",')
-    print('    "image": "https://4kwallpapers.com/images/wallpapers/kitty-couple-kawaii-1920x1080-10102.png"')
+    print('    "content": "",')
+    print('    "image": ""')
     print('},')
